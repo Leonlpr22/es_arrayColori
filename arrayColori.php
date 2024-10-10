7456e9a390397fe40872d59e8a8fb27138bd2f42
Leon Lo Preiato <leonlopreiato@Leons-MBP.wind3.hub>
2024-10-10 06:11:05 +0200

    non colora

---
+++ arrayColori.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        $num = rand(10, 20);
+
+        $lista = [];
+        for ($i = 0; $i <= $num; $i++) {
+            $lista[] = $i; // Aggiungere il numero all'array
+        }
+
+        $colori = ["rosso", "verde", "blu", "giallo", "arancione"];
+
+        echo "<ol>";
+
+        foreach ($lista as $numero) {
+            $coloreCasuale = $colori[array_rand($colori)];
+            
+            echo "<li style= 'color: $coloreCasuale;'>$numero</li>";
+        }
+
+        echo "</ol>";
+    ?>
+
+</body>
+</html>
